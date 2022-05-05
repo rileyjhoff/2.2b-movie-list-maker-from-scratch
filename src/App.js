0c6a2ef8404cc2b20758d9e559cd786a9ec459bb
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-05 11:52:52 -0700

    initialized state in App

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import './App.css';
 import Movie from './components/Movie';
 import MovieForm from './components/MovieForm';
 import MovieList from './components/MovieList';
+import { useState } from 'react';
 
 function App() {
+  const [allMovies, setAllMovies] = useState([]);
+  const [filter, setFilter] = useState('');
+  const [filteredMovies, setFilteredMovies] = useState([]);
+  const [movieFormTitle, setMovieFormTitle] = useState('');
+  const [movieFormYear, setMovieFormYear] = useState('');
+  const [movieFormDirector, setMovieFormDirector] = useState('');
+  const [movieFormColor, setMovieFormColor] = useState('');
+
   return (
     <div className="App">
       <MovieForm />
