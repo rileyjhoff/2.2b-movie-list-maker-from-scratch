98838ebf5a4422ccf057a0d493c75dca4587ac6c
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-05 13:01:03 -0700

    Movie card only renders if one of title, director, or year has a value

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,10 +3,14 @@ import { Card } from 'react-bootstrap';
 
 export default function Movie({ year, title, director, color }) {
   return (
-    <Card className="movie-card" style={{ backgroundColor: color }}>
-      <Card.Title>{title}</Card.Title>
-      <Card.Text>{year}</Card.Text>
-      <Card.Text>{director}</Card.Text>
-    </Card>
+    <div>
+      {(title || director || year) && (
+        <Card className="movie-card" style={{ backgroundColor: color }}>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{year}</Card.Text>
+          <Card.Text>{director}</Card.Text>
+        </Card>
+      )}
+    </div>
   );
 }
