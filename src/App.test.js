3936bb9253996d677e7b1d11f13d47aaea39311f
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-06 10:11:01 -0700

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test('renders learn react link', () => {
-  // render(<App />);
-  // const textElement = screen.getByText(/learn react/i);
-  // expect(textElement).toBeInTheDocument();
+test('renders Movie Card text', () => {
+  render(<App />);
+  const textElement = screen.getByText(/Movie Card/i);
+  expect(textElement).toBeInTheDocument();
 });
