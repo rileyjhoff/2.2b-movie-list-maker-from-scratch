ee905b543b95279f5257da7ce137b6e57b4d4465
Riley Hoffman <rileyjhoff@gmail.com>
2022-05-06 09:30:00 -0700

    fixed handleDeleteMovie call in Movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,7 +5,11 @@ export default function Movie({ year, title, director, color, handleDeleteMovie 
   return (
     <div>
       {(title || director || year) && (
-        <Card className="movie-card" style={{ backgroundColor: color }} onClick={handleDeleteMovie}>
+        <Card
+          className="movie-card"
+          style={{ backgroundColor: color }}
+          onClick={() => handleDeleteMovie(title)}
+        >
           <Card.Title>{title}</Card.Title>
           <Card.Text>{year}</Card.Text>
           <Card.Text>{director}</Card.Text>
